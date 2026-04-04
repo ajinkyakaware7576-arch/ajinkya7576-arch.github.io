6abd61b688c9c2cfa8bfe4cf9db788ddc928e519
Ajinkyak_2829 <ajinkyakaware7576@gmail.com>
2026-04-04 19:12:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,38 @@ toggle.addEventListener("click", () => {
   } else {
     toggle.innerText = "🌙";
   }
+  const searchInput = document.querySelector(".search-wrap input");
+const cards = document.querySelectorAll(".card");
+
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.toLowerCase();
+
+  cards.forEach(card => {
+    const text = card.innerText.toLowerCase();
+
+    if (text.includes(value)) {
+      card.style.display = "flex";
+    } else {
+      card.style.display = "none";
+    }
+    const noResults = document.getElementById("noResults");
+
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.toLowerCase();
+  let visible = 0;
+
+  cards.forEach(card => {
+    const text = card.innerText.toLowerCase();
+
+    if (text.includes(value)) {
+      card.style.display = "flex";
+      visible++;
+    } else {
+      card.style.display = "none";
+    }
+  });
+
+  noResults.style.display = visible === 0 ? "block" : "none";
 });
+
+
