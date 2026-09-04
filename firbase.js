cc13b3a992073fa79a6e660b1e04f0de3e64e576
Ajinkyak_2829 <ajinkyakaware7576@gmail.com>
2026-09-04 20:56:38 +0530

    Create firbase.js

---
+++ firbase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDIz0YzbSQ1Y_V3cnkHBzfPsvlwdBej4A",
+  authDomain: "study-together-7c54f.firebaseapp.com",
+  databaseURL: "https://study-together-7c54f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "study-together-7c54f",
+  storageBucket: "study-together-7c54f.firebasestorage.app",
+  messagingSenderId: "47387281136",
+  appId: "1:47387281136:web:1bfe0c667af2692af82435"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
